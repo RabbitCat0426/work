27c098d9f63e845a43c3dec2b97e563e9f72afd6
rb <usagineko4426@gmail.com>
2024-10-03 22:08:27 +0900

    newcommit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="title">タイトル</div>
+      </header>
+      <body className="App-body">
+        <form>
+          <select className="MainPageSelect" id='op'>
+            <option id="op0">一次方程式</option>
+            <option id="op1">二次方程式</option>
+          </select>
+          <button id='actButton' onClick={act(this)}>実行</button>
+        </form>
+      </body>
+    </div>
+  );
+}
+
+function act(){
+  var i = 'test'
+  console.log(i);
+}
+
